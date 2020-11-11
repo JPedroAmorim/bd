c18dfd8ab4fdd7afa6f072a7992d7144309033ef
João Pedro <joaopdamorim@gmail.com>
2020-11-11 16:10:01 -0300

    login cntrlr

--- src/main/java/com/macrochallenge/backend/controllers/LoginController.java
+++ src/main/java/com/macrochallenge/backend/controllers/LoginController.java
@@ -1,20 +1,31 @@
 package com.macrochallenge.backend.controllers;
 
+import com.macrochallenge.backend.model.dto.ExistsDTO;
 import com.macrochallenge.backend.model.dto.UserDTO;
+import com.macrochallenge.backend.service.LoginService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("api/v1/login")
 @RestController
 public class LoginController {
 
+    private final LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
     @PostMapping
     public ResponseEntity<String> addNewUser(@RequestBody UserDTO userDTO) {
+        loginService.addNewUser(userDTO.getUserId());
         return new ResponseEntity<>("Usuário adicionado com sucesso", HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ExistsDTO checkUser(@RequestBody UserDTO userDTO) {
+        return loginService.checkUserExists(userDTO.getUserId());
     }
 
 }
